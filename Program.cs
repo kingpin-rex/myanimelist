bc1dd8955c84eed0706f2ba26c02031e41a6ccec
kingpin-rex <rexonyeukwu14@gmail.com>
2026-07-06 15:29:44 +0100

    Subscription requests
    This is a small exercise that emulates the subscription requests of a streaming service.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration == 0)
+    {
+        Console.WriteLine("Your subscription has expired.");
+        discountPercentage = 0;
+    }
+
+    else if (daysUntilExpiration == 1)
+    {
+        discountPercentage = 20;
+        Console.WriteLine($@"Your subscription expires within a day!
+Renew now and save {discountPercentage}%!");
+    }
+
+    else if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
+    }
+
+    else 
+    {
+        discountPercentage = 0;
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+}
+
